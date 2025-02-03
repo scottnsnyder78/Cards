b897fd7beead79e32cab72c4c94247e3482c5b9f
Scott <scottnsnyder78@gmail.com>
2025-02-03 18:36:33 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Scott Snyder on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
